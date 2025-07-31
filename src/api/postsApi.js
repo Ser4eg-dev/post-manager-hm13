aff04af0a03e85e2d16689c8179ef06605ad1279
Serhii <ser4eg.dev@gmail.com>
2025-07-31 14:48:44 +0300

    Update postsApi.js

--- src/api/postsApi.js
+++ src/api/postsApi.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const postsApi = createApi({
   reducerPath: 'postsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://post-manager-server.onrender.com/api' }),
   tagTypes: ['Post', 'Posts'],
   endpoints: build => ({
     // Список постів з пагінацією (build.query)
